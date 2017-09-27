830c658d00e6b422e5b8c6e68861bec187a740a2
Igor <gafan94@yandex.ru>
2017-09-27 22:57:24 +0300

    introduce flow type checker

--- src/index.js
+++ src/index.js
@@ -1,9 +1,8 @@
+// @flow
+
 /**
  * Add two numbers.
- * @param {number} num1 The first number.
- * @param {number} num2 The second number.
- * @returns {number} The sum of the two numbers.
  */
-export function add (num1, num2) {
+export function add (num1: number, num2: number): number {
     return num1 + num2;
 }
